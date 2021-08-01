02d5eaae2139ea994d5029fe1b7bfee0759b66f5
rick <ricardrao.rbo@gmail.com>
2021-08-01 20:33:53 +0100

    Issue: Added generator.rs
    * Added generator.rs to the project tree

---
+++ src/file_exporter/generator.rs
@@ -0,0 +1,8 @@
+pub fn generated_header_message() -> String{
+    return "/* This code was auto generated
+ * This generator is work in progress project
+ * 
+ * report any bug to the author of this generator
+ * Github: https://github.com/RetroRick/snippet_generator/
+ */".to_string();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod file_parser;
 mod tokenizer;
+mod file_exporter;
 
 fn main() {
     let mut _new_parser =  file_parser::FileParser::new("mystruct.dl".to_string());
     _new_parser.get_raw_data_from_file();
     let my_tokens = tokenizer::tokenize(&_new_parser.get_raw_data());
     tokenizer::print_tokens(&my_tokens);
+    file_exporter::openfile();
 }
